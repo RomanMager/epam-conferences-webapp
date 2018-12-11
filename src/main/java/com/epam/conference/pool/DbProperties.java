37de750e5c51aaecc8882539e4e44c841b079c1a
Roman <roman.mager@outlook.com>
2018-12-11 21:40:02 +0300

    Implement methods readProperties() and readUrl()
    - use properties.setProperty() instead of loading them using load(), because it cannot load resource and throw NullPointerException.

--- src/main/java/com/epam/conference/pool/DbProperties.java
+++ src/main/java/com/epam/conference/pool/DbProperties.java
@@ -1,4 +1,34 @@
 package com.epam.conference.pool;
 
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 public class DbProperties {
+    private static ResourceBundle bundle;
+    
+    static {
+        bundle = ResourceBundle.getBundle("database");
+    }
+    
+    private DbProperties() {
+    }
+    
+    // TODO: rename?
+    public static Properties readProperties() {
+        Properties properties = new Properties();
+        
+        properties.setProperty("user", bundle.getString("db.user"));
+        properties.setProperty("password", bundle.getString("db.password"));
+        properties.setProperty("useSSL", bundle.getString("db.useSSL"));
+        properties.setProperty("autoReconnect", bundle.getString("db.autoReconnect"));
+        properties.setProperty("useUnicode", bundle.getString("db.useUnicode"));
+        properties.setProperty("characterEncoding", bundle.getString("db.characterEncoding"));
+        
+        return properties;
+    }
+    
+    // TODO: rename?
+    public static String readUrl() {
+        return bundle.getString("db.url");
+    }
 }
