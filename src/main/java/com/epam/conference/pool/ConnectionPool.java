48f0ab0d509df9e3c66174024e8c612dc647f10d
Roman <roman.mager@outlook.com>
2018-12-12 01:29:00 +0300

    Create ConnectionPool class
    - the class is Singleton.
    - setUpConnection() adds connections to connections queue.
    - getConnection() retrieves connection from connections queue.
    - releaseConnection() returns connection into queue.
    - destroy() closes connection.
    
    Issue: taken connections are not closed by destroy()

---
+++ src/main/java/com/epam/conference/pool/ConnectionPool.java
@@ -0,0 +1,108 @@
+package com.epam.conference.pool;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
+
+// TODO: add <T> ???
+@Log4j2
+public class ConnectionPool {
+    private static final int POOL_SIZE = 10;
+    // TODO: is ik okay to initialize field here?
+    private static AtomicBoolean instanceCreated = new AtomicBoolean(false);
+    private static ConnectionPool instance;
+    private static ReentrantLock locker = new ReentrantLock();
+    
+    // Register DB drivers
+    static {
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        } catch (SQLException e) {
+            log.fatal("Database drivers cannot be loaded." + e);
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private BlockingQueue<ProxyConnection> connectionQueue;
+    
+    private ConnectionPool() {
+        // TODO: init in constructor or when the field is declared?
+        connectionQueue = new LinkedBlockingQueue<>(POOL_SIZE);
+        setUpConnection();
+    }
+    
+    public static ConnectionPool getInstance() {
+        if (!instanceCreated.get()) {
+            locker.lock();
+            try {
+                if (!instanceCreated.get()) {
+                    instance = new ConnectionPool();
+                    instanceCreated.set(true);
+                }
+            } finally {
+                locker.unlock();
+            }
+        }
+        
+        return instance;
+    }
+    
+    private void setUpConnection() {
+        for (int i = 0; i <= POOL_SIZE; i++) {
+            try {
+                ProxyConnection connection = new ProxyConnection(DriverManager.getConnection(
+                        DbProperties.readUrl(),
+                        DbProperties.readProperties()
+                ));
+                
+                // todo: user 'offer' or 'add' ?
+                connectionQueue.offer(connection);
+            } catch (SQLException e) {
+                log.error("Unable to initialize Connection Pool.");
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    public ProxyConnection getConnection() {
+        ProxyConnection connection = null;
+        try {
+            connection = connectionQueue.take();
+        } catch (InterruptedException e) {
+            log.error("Cannot retrieve connection.");
+        }
+        return connection;
+    }
+    
+    public void releaseConnection(ProxyConnection connection) {
+        connectionQueue.offer(connection);
+    }
+    
+    // deregister drivers
+    public void destroy() {
+        try {
+            for (int i = 0; i <= POOL_SIZE; i++) {
+                ProxyConnection connection = connectionQueue.take();
+                connection.closeConnection();
+            }
+        } catch (InterruptedException | SQLException e) {
+            log.error("Error occurred while closing connections." + e);
+        }
+        
+        DriverManager.drivers().forEach(driver -> {
+            try {
+                DriverManager.deregisterDriver(driver);
+            } catch (SQLException e) {
+                log.error("Error occurred while deregister drivers." + e);
+            }
+        });
+        
+        log.info("Connections are successfully closed. Drivers have been deregistered.");
+    }
+}
+
