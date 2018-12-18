64bf40f80b06c1c3256e715c24784613dfa79323
Roman <roman.mager@outlook.com>
2018-12-19 02:36:56 +0300

    Add Router class that defines application main routes

---
+++ src/main/java/com/epam/conference/controller/Router.java
@@ -0,0 +1,6 @@
+package com.epam.conference.controller;
+
+public class Router {
+    public static final String PAGE_LOGIN = "/views/login.jsp";
+    public static final String PAGE_MAIN = "/views/main.jsp";
+}
