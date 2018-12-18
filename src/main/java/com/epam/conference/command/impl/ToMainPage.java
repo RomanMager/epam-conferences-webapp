8b39e231f448a9b5b8fa5d6981c1bfefc6647b40
Roman <roman.mager@outlook.com>
2018-12-19 02:22:12 +0300

    Add command classes that implement Command interface

---
+++ src/main/java/com/epam/conference/command/impl/ToMainPage.java
@@ -0,0 +1,12 @@
+package com.epam.conference.command.impl;
+
+import com.epam.conference.command.Command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ToMainPage implements Command {
+    @Override
+    public String execute(HttpServletRequest request) {
+        throw new UnsupportedOperationException();
+    }
+}
