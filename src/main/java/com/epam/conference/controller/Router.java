36c6026f139cd3223af8e1182550d0b7028507d6
Roman <roman.mager@outlook.com>
2018-12-19 21:10:52 +0300

    Add private constructor in Router class

--- src/main/java/com/epam/conference/controller/Router.java
+++ src/main/java/com/epam/conference/controller/Router.java
@@ -3,4 +3,7 @@ package com.epam.conference.controller;
 public class Router {
     public static final String PAGE_LOGIN = "/views/login.jsp";
     public static final String PAGE_MAIN = "/views/main.jsp";
+    
+    private Router() {
+    }
 }
