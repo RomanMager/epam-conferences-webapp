26c3c948f6a8a99e840eacdbfe2ef401de938eda
Roman <roman.mager@outlook.com>
2018-12-22 00:03:06 +0300

    Update ToLoginPage execute method
    Now it redirects to the main page.

--- src/main/java/com/epam/conference/command/impl/ToLoginPage.java
+++ src/main/java/com/epam/conference/command/impl/ToLoginPage.java
@@ -1,12 +1,13 @@
 package com.epam.conference.command.impl;
 
 import com.epam.conference.command.Command;
+import com.epam.conference.controller.Router;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class ToLoginPage implements Command {
     @Override
     public String execute(HttpServletRequest request) {
-        throw new UnsupportedOperationException();
+        return Router.PAGE_LOGIN;
     }
 }
