9ad90856a00ea41f51ec267dcafc79bde300b9a7
Roman <roman.mager@outlook.com>
2018-12-25 16:09:50 +0300

    Create participant service class

---
+++ src/main/java/com/epam/conference/service/ParticipantService.java
@@ -0,0 +1,10 @@
+package com.epam.conference.service;
+
+import com.epam.conference.entity.Person;
+
+public class ParticipantService {
+    public Person findParticipant(String login, String email, String password) {
+        throw new UnsupportedOperationException();
+    }
+
+}
