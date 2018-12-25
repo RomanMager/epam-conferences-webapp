3c313dc62c7a27d9d17d11aabf428b5d1c1b143d
Roman <roman.mager@outlook.com>
2018-12-25 16:08:34 +0300

    Modify Register execute method
    It just gets request params.
    Method still throws UnsupportedOperationException().

--- src/main/java/com/epam/conference/command/impl/Register.java
+++ src/main/java/com/epam/conference/command/impl/Register.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 public class Register implements Command {
     @Override
     public String execute(HttpServletRequest request) {
+        String login = request.getParameter("login");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
         throw new UnsupportedOperationException();
     }
 }
