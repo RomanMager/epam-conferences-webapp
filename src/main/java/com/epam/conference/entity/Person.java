dd7a639a9fe94b21b8c3606f6d357fcd95dafad5
Roman <roman.mager@outlook.com>
2018-12-26 04:17:11 +0300

    Add constructor in Person class

--- src/main/java/com/epam/conference/entity/Person.java
+++ src/main/java/com/epam/conference/entity/Person.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true) // TODO: Really need to call super? ('Entity' class is empty)
 public class Person extends Entity {
     private int personId;
     private String login;
@@ -16,5 +16,17 @@ public class Person extends Entity {
     private String password;
     private String name;
     private String surname;
-    private RoleName roleName;
+    // TODO: not sure how we can supply this field using a registration form
+    //  - participants definitely should not be able to register as 'admins'
+    //  - or the field can be set automatically, if nothing is provided
+    private RoleName role;
+
+    public Person(String login, String email, String password) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+    }
+
 }
+
+
