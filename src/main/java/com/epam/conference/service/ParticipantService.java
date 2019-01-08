c7331ff87a10fcbd7c5721873f80252e733f0fee
Roman <roman.mager@outlook.com>
2019-01-09 01:09:16 +0300

    Add catch clause in method findParticipant()

--- src/main/java/com/epam/conference/service/ParticipantService.java
+++ src/main/java/com/epam/conference/service/ParticipantService.java
@@ -1,14 +1,22 @@
 package com.epam.conference.service;
 
-import com.epam.conference.dao.impl.ParticipantDAOImpl;
+import com.epam.conference.dao.impl.ParticipantDAO;
 import com.epam.conference.entity.Person;
 import com.epam.conference.exception.DAOException;
+import com.epam.conference.exception.ServiceException;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
 
 @Log4j2
 public class ParticipantService {
     public Person findParticipant(String login, String password) {
-        Person person = ParticipantDAOImpl.getInstance().findParticipantByLoginPassword(login, password);
+        Person person = null;
+        try {
+            person = ParticipantDAO.getInstance().findParticipantByLoginPassword(login, password);
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
         return person;
     }
 
@@ -22,12 +30,22 @@ public class ParticipantService {
     public void createParticipant(Person person) {
         // TODO: VALIDATION
         //      - Check if already exists in DB
-        ParticipantDAOImpl participantDAO = ParticipantDAOImpl.getInstance();
+        ParticipantDAO participantDAO = ParticipantDAO.getInstance();
         try {
             participantDAO.add(person);
         } catch (DAOException e) {
             // TODO: TROW exception or LOG exception?
             log.error(e);
         }
+    }
+
+    public List<Person> getAllParticipants() throws ServiceException {
+        List<Person> personList;
+        try {
+            personList = ParticipantDAO.getInstance().findAll();
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return personList;
     }
 }
