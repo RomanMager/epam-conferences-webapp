fdf938e9413c7be02c7ceab998e9166ceb868bb7
Roman <roman.mager@outlook.com>
2019-01-11 23:06:54 +0300

    Create class ParticipantData with fields 'name', 'surname'

---
+++ src/main/java/com/epam/conference/entity/ParticipantData.java
@@ -0,0 +1,15 @@
+package com.epam.conference.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ParticipantData extends Entity {
+    private String name;
+    private String surname;
+}
