0b4f8b667259314bd79af574decddaab4a68cffc
Roman <roman.mager@outlook.com>
2019-01-11 23:07:27 +0300

    Remove fields 'name', 'surname'
    They were transferred to class ParticipantData.

--- src/main/java/com/epam/conference/entity/Person.java
+++ src/main/java/com/epam/conference/entity/Person.java
@@ -14,21 +14,12 @@ public class Person extends Entity {
     private String login;
     private String email;
     private String password;
-    private String name;
-    private String surname;
     // TODO: not sure how we can supply this field using a registration form
     //  - participants definitely should not be able to register as 'admins'
     //  - or the field can be set automatically, if nothing is provided
     private RoleName role;
 
     public Person(String login, String email, String password) {
-        this.login = login;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Person(int personId, String login, String email, String password) {
-        this.personId = personId;
         this.login = login;
         this.email = email;
         this.password = password;
