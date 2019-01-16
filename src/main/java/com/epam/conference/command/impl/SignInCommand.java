1c2cf8e7b82d06763d75cef1c07c26312ae26abe
Roman <roman.mager@outlook.com>
2019-01-17 00:56:51 +0300

    Refactor SignIn command to catch exceptions

--- src/main/java/com/epam/conference/command/impl/SignInCommand.java
+++ src/main/java/com/epam/conference/command/impl/SignInCommand.java
@@ -3,6 +3,7 @@ package com.epam.conference.command.impl;
 import com.epam.conference.command.Command;
 import com.epam.conference.controller.Router;
 import com.epam.conference.entity.Person;
+import com.epam.conference.exception.ServiceException;
 import com.epam.conference.service.ParticipantService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,22 +14,28 @@ public class SignInCommand implements Command {
     public String execute(HttpServletRequest request) {
         String page;
         String login = request.getParameter("login");
-        String email = request.getParameter("email");
         String password = request.getParameter("password");
+        // String email = request.getParameter("email");
 
         ParticipantService participantService = new ParticipantService();
 
-        Person participant = participantService.findParticipant(login, password);
+        Person participant;
+        try {
+            participant = participantService.findParticipant(login, password);
 
-        if (participant != null) {
-            HttpSession session = request.getSession();
-            session.setAttribute("login", participant.getLogin());
-            session.setAttribute("role", participant.getRole());
-            session.setAttribute("personId", participant.getPersonId());
-            page = Router.PAGE_PROFILE;
-        } else {
-            request.setAttribute("error", "Incorrect login or password");
-            page = Router.PAGE_MAIN; // TODO: replace with error page
+            if (participant != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("login", participant.getLogin());
+                session.setAttribute("role", participant.getRole().getRoleName());
+                session.setAttribute("personId", participant.getPersonId());
+                page = Router.PAGE_PROFILE;
+            } else {
+                request.setAttribute("error", "Incorrect login or password");
+                page = Router.PAGE_ERROR;
+            }
+        } catch (ServiceException e) {
+            request.setAttribute("error", e.getMessage());
+            page = Router.PAGE_ERROR;
         }
 
         return page;
