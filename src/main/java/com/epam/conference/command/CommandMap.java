fc5d218d9f32a2048b5152196d557004ef69ba66
Roman <roman.mager@outlook.com>
2019-01-17 21:34:32 +0300

    Rename ToCreateConferencePageCommand

--- src/main/java/com/epam/conference/command/CommandMap.java
+++ src/main/java/com/epam/conference/command/CommandMap.java
@@ -4,7 +4,7 @@ import com.epam.conference.command.impl.GetUsersCommand;
 import com.epam.conference.command.impl.RegisterCommand;
 import com.epam.conference.command.impl.SignInCommand;
 import com.epam.conference.command.impl.SignOutCommand;
-import com.epam.conference.command.impl.admin.CreateConference;
+import com.epam.conference.command.impl.admin.CreateConferenceCommand;
 import com.epam.conference.command.impl.routing.*;
 
 public enum CommandMap {
@@ -12,12 +12,12 @@ public enum CommandMap {
     TO_REGISTER_PAGE(new ToRegisterPageCommand()),
     TO_LOGIN_PAGE(new ToLoginPageCommand()),
     TO_PROFILE_PAGE(new ToProfilePageCommand()),
-    TO_CREATE_CONFERENCE_PAGE(new ToCreateConferencePage()),
+    TO_CREATE_CONFERENCE_PAGE(new ToCreateConferencePageCommand()),
     REGISTER(new RegisterCommand()),
     SIGN_IN(new SignInCommand()),
     SIGN_OUT(new SignOutCommand()),
     GET_USERS(new GetUsersCommand()),
-    CREATE_CONFERENCE(new CreateConference());
+    CREATE_CONFERENCE(new CreateConferenceCommand());
 
     private final Command command;
 
--- src/main/java/com/epam/conference/command/impl/routing/ToCreateConferencePageCommand.java
+++ src/main/java/com/epam/conference/command/impl/routing/ToCreateConferencePageCommand.java
@@ -5,7 +5,7 @@ import com.epam.conference.controller.Router;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class ToCreateConferencePage implements Command {
+public class ToCreateConferencePageCommand implements Command {
     @Override
     public String execute(HttpServletRequest request) {
         return Router.PAGE_CREATE_CONFERENCE;
